75411d1905ff07b69d3ebd1654f28600cf40efd5
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2021-07-18 15:14:24 +0900

    add:TestSchedules
    スケジュールモデルのテストモデル作成しました

---
+++ src/database/seeders/TestSchedules.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class TestSchedules extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('schedules')->insert([
+            'title' => 'test title 1',
+            'content' => 'test content 1',
+            'scheduledDate' => '2021-9-1',
+            'user_id' => 1
+        ]);
+        DB::table('schedules')->insert([
+            'title' => 'test title 2',
+            'content' => 'test content 2',
+            'scheduledDate' => '2022-5-8',
+            'user_id' => 1
+        ]);
+        DB::table('schedules')->insert([
+            'title' => 'test title 3',
+            'content' => 'test content 3',
+            'scheduledDate' => '2020-12-3',
+            'user_id' => 1
+        ]);
+        DB::table('schedules')->insert([
+            'title' => 'test title 4',
+            'content' => 'test content 4',
+            'scheduledDate' => '2021-8-4',
+            'user_id' => 1
+        ]);
+        DB::table('schedules')->insert([
+            'title' => 'test title 5',
+            'content' => 'test content 5',
+            'scheduledDate' => '2020-8-7',
+            'user_id' => 1
+        ]);
+        DB::table('schedules')->insert([
+            'title' => 'test title 6',
+            'content' => 'test content 6',
+            'scheduledDate' => '2022-1-1',
+            'user_id' => 1
+        ]);
+    }
+}
